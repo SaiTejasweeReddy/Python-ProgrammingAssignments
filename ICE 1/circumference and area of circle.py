8a3255f1132d31f32ba418d382eb74e59cf8fac3
SaiTejasweeReddy <snigdhareddy.95@gmail.com>
2017-06-09 10:47:52 -0700

    ICE 1

---
+++ ICE 1/circumference and area of circle.py
@@ -0,0 +1,7 @@
+import math
+radString = input('Please enter the radius of circle: ')
+rad = int(radString)
+cir = 2 * math.pi * rad
+print('circumference of the circle is ',cir)
+area = math.pi * rad * rad
+print('area of the circle is ',area)

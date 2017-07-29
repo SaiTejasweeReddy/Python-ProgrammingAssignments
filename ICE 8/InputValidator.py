57a35705522793b4db797b081af83c7998d3f034
SaiTejasweeReddy <snigdhareddy.95@gmail.com>
2017-07-29 16:57:11 -0500

    ICE8

---
+++ ICE 8/InputValidator.py
@@ -0,0 +1,26 @@
+def validateGender(x):
+    try:
+        return x.lstrip()!='b' and x.lstrip()!='g'
+    except ValueError:
+        return False
+def validateOrder(x):
+    try:
+        return x<1
+    except ValueError:
+        return False
+def validateMinimumLength(x):
+    try:
+        return x<1
+    except ValueError:
+        return False
+
+def validateMaximumLength(x):
+    try:
+        return x>20
+    except ValueError:
+        return False
+def validateCount(x):
+    try:
+        return x>1000
+    except ValueError:
+        return False
